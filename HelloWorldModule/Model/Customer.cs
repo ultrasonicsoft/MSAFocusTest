a7c5b1bf5a1d1fecb1d1c8abc01bc12e8456eb77
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-04-07 23:32:49 +0530

    Data is loading from LoadData button command

---
+++ HelloWorldModule/Model/Customer.cs
@@ -0,0 +1,13 @@
+namespace HelloWorldModule.Utils
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
--- HelloWorldModule/ViewModel/MainViewModel.cs
+++ HelloWorldModule/ViewModel/MainViewModel.cs
@@ -1,20 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using HelloWorldModule.Repository;
 using HelloWorldModule.Utils;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace HelloWorldModule.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel  : BindableBase
     {
         private ICustomerService customerService = new CustomerService();
-        public IList<Customer> Customers { get; set; }
+
+        private ObservableCollection<Customer> _customers;
+        public ObservableCollection<Customer> Customers
+        {
+            get { return this._customers; }
+            set { SetProperty(ref this._customers, value); }
+        }
+
+        private string _test;
+        public string Test
+        {
+            get { return this._test; }
+            set { SetProperty(ref this._test, value); }
+        }
+
+        //public string Test { get; private set; }
+        public ICommand LoadCustomersCommand { get; private set; }
         public MainViewModel()
         {
-            Customers = customerService.GetAllCustomer();
+            Test = "Balram";
+            Customers = new ObservableCollection<Customer>();
+            this.LoadCustomersCommand = new DelegateCommand<object>(
+                                   this.OnLoadCustomersCommand, this.CanLoadCustomersCommand);
+
+        }
+
+        private bool CanLoadCustomersCommand(object arg)
+        {
+            return true;
+        }
+
+        private void OnLoadCustomersCommand(object obj)
+        {
+            Test = "Sujeet";
+            Customers = new ObservableCollection<Customer>(customerService.GetAllCustomer());
         }
     }
 }
